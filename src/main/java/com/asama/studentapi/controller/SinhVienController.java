764c9944abb1e257b38356637125424f7126ce9a
asamacode <trhuuluong@gmail.com>
2020-04-07 15:12:01 +0700

    complete

--- src/main/java/com/asama/studentapi/controller/SinhVienController.java
+++ src/main/java/com/asama/studentapi/controller/SinhVienController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,37 +20,80 @@ public class SinhVienController {
 
 	@Autowired
 	SinhVienService service;
-	
+
 	@RequestMapping(value = "/getall")
 	public List<SinhVien> getAllSinhVien() {
 		return service.getAllDataSinhViens();
 	}
-	
+
 	@RequestMapping(value = "/getsv")
 	public Optional<SinhVien> getSingleSinhVien(@RequestParam(name = "id") Integer id) {
 		return service.getSinhVien(id);
 	}
-	
+
 	@RequestMapping(value = "/getsvbyname")
 	public List<SinhVien> getSinhVienByName(@RequestParam(value = "name") String name) {
 		return service.gerAllSinhVienByName(name);
 	}
-	
+
 	@RequestMapping(value = "/insert")
 	public boolean insertOneSV(@RequestBody SinhVien sinhVien) {
 		service.insertOneSinhVien(sinhVien);
 		return true;
 	}
-	
+
 	@RequestMapping(value = "/insertmany")
 	public boolean inserManySV(@RequestBody List<SinhVien> sinhViens) {
 		service.insertManySinhVien(sinhViens);
 		return true;
 	}
-	
-	@RequestMapping(value = "/update") 
+
+	@RequestMapping(value = "/update")
 	public boolean updateSV(@RequestBody SinhVien sinhVien) {
 		service.updateSV(sinhVien);
 		return true;
+	}
+
+	@RequestMapping(value = "/check")
+	public boolean checkSinhVienExists(@RequestBody SinhVien sinhVien) {
+		return service.checkSinhVienExists(sinhVien.getId());
+	}
+
+	@RequestMapping(value = "/delete")
+	public boolean deleteOneSV(@RequestBody SinhVien sinhVien) {
+		service.deleteOneSinhVien(sinhVien.getId());
+		return true;
+	}
+
+	@RequestMapping(value = "/deletebyname")
+	public boolean deleteByName(@RequestBody SinhVien sinhVien) {
+		service.deleteByName(sinhVien.getName());
+		return true;
+	}
+
+	@RequestMapping(value = "/getallsort")
+	public List<SinhVien> getAllSVSortDesc() {
+		return service.getListSVSortById();
+	}
+	
+	@RequestMapping(value = "/allsv")
+	public Page<SinhVien> get10SV(@PageableDefault(page = 1) @RequestParam(name = "page") Integer page) {
+		return service.get10SinhVienPerPage(page);
+	}
+	
+	@RequestMapping(value = "/findbylastname")
+	public List<SinhVien> getSVByLastName(@RequestParam(name = "lastname") String lastName) {
+		return service.getAllSinhVienByLastName(lastName);
+	}
+	
+	@RequestMapping(value = "/deletebynameandaddress")
+	public boolean deleteByNameAndAddress(@RequestBody SinhVien sinhVien) {
+		service.deleteAllSVByNameAndAddress(sinhVien);
+		return true;
+	}
+	
+	@RequestMapping(value = "/findbyaddress") 
+	public List<SinhVien> getSinhVienByAddress(@RequestParam(name = "address") String address) {
+		return service.getAllSVByAddressAndSortAscByName(address);
 	}
 }
--- src/main/java/com/asama/studentapi/model/service/SinhVienService.java
+++ src/main/java/com/asama/studentapi/model/service/SinhVienService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.asama.studentapi.model.bean.SinhVien;
@@ -29,21 +32,77 @@ public class SinhVienService {
 		SinhVien sinhVien = new SinhVien();
 		sinhVien.setName(name);
 
-		ExampleMatcher matcher = ExampleMatcher.matching()
-				.withIgnorePaths("id").withIgnorePaths("address");
+		ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("id").withIgnorePaths("address");
 
 		return sinhVienDAO.findAll(Example.of(sinhVien, matcher));
 	}
-	
+
 	public void insertOneSinhVien(SinhVien sinhVien) {
 		sinhVienDAO.save(sinhVien);
 	}
-	
+
 	public void insertManySinhVien(List<SinhVien> sinhViens) {
 		sinhVienDAO.saveAll(sinhViens);
 	}
-	
+
 	public void updateSV(SinhVien sinhVien) {
 		sinhVienDAO.save(sinhVien);
+	}
+
+	public boolean checkSinhVienExists(Integer id) {
+		return sinhVienDAO.existsById(id);
+	}
+
+	public void deleteOneSinhVien(Integer id) {
+		sinhVienDAO.deleteById(id);
+	}
+
+	public void deleteByName(String name) {
+		SinhVien sinhVien = new SinhVien();
+		sinhVien.setName(name);
+
+		ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("id").withIgnorePaths("address");
+
+		List<SinhVien> sinhViens = sinhVienDAO.findAll(Example.of(sinhVien, matcher));
+
+		sinhVienDAO.deleteAll(sinhViens);
+	}
+	
+	public List<SinhVien> getListSVSortById() {
+		return sinhVienDAO.findAll(Sort.by("id").descending());
+	}
+	
+	public Page<SinhVien> get10SinhVienPerPage(Integer page) {
+		return sinhVienDAO.findAll(PageRequest.of(page, 10));
+	}
+	
+	public List<SinhVien> getAllSinhVienByLastName(String lastName) {
+		SinhVien sinhVien = new SinhVien();
+		sinhVien.setName(lastName);
+		
+		ExampleMatcher matcher = ExampleMatcher.matching()
+				.withIgnorePaths("id")
+				.withIgnorePaths("address")
+				.withMatcher("name", match -> match.endsWith());
+		
+		return sinhVienDAO.findAll(Example.of(sinhVien, matcher));
+	}
+	
+	public void deleteAllSVByNameAndAddress(SinhVien sinhVien) {
+		ExampleMatcher matcher = ExampleMatcher.matching()
+				.withIgnorePaths("id");
+		List<SinhVien> sinhViens = sinhVienDAO.findAll(Example.of(sinhVien, matcher));
+		sinhVienDAO.deleteAll(sinhViens);		
+	}
+	
+	public List<SinhVien> getAllSVByAddressAndSortAscByName(String address) {
+		SinhVien sinhVien = new SinhVien();
+		sinhVien.setAddress(address);
+		
+		ExampleMatcher matcher = ExampleMatcher.matching()
+				.withIgnorePaths("id")
+				.withIgnorePaths("name");
+		
+		return sinhVienDAO.findAll(Example.of(sinhVien, matcher), Sort.by("name").ascending());
 	}
 }
